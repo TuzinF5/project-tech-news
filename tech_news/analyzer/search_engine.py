967e010eff4a5aa3d07899345676be140c7581ea
Arthur Teixeira <tuzinf5@gmail.com>
2022-11-08 19:09:18 -0300

    feat(function): :sparkles: Creating logic for search_by_tag function

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -20,7 +20,15 @@ def search_by_date(date):
 
 # Requisito 8
 def search_by_tag(tag):
-    """Seu código deve vir aqui"""
+    notices = find_news()
+    response = []
+
+    for notice in notices:
+        for tag_notice in notice["tags"]:
+            if tag.upper() in tag_notice.upper():
+                response.append((notice["title"], notice["url"]))
+
+    return response
 
 
 # Requisito 9
