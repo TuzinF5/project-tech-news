b166af6bebae6e0e697cda96ff00d30d1b6ec2c7
Arthur Teixeira <tuzinf5@gmail.com>
2022-11-09 16:35:29 -0300

    feat(function): :sparkles: Implements input logic in analyzer_menu function

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,3 +1,29 @@
+import sys
+
+
 # Requisito 12
 def analyzer_menu():
-    """Seu código deve vir aqui"""
+    displays_the_options = {
+        "0": "Digite quantas notícias serão buscadas:",
+        "1": "Digite o título:",
+        "2": "Digite a data no formato aaaa-mm-dd:",
+        "3": "Digite a tag:",
+        "4": "Digite a categoria:",
+    }
+
+    options_answer = input(
+        "Selecione uma das opções a seguir:\n"
+        " 0 - Popular o banco com notícias;\n"
+        " 1 - Buscar notícias por título;\n"
+        " 2 - Buscar notícias por data;\n"
+        " 3 - Buscar notícias por tag;\n"
+        " 4 - Buscar notícias por categoria;\n"
+        " 5 - Listar top 5 notícias;\n"
+        " 6 - Listar top 5 categorias;\n"
+        " 7 - Sair.\n"
+    )
+
+    try:
+        input(displays_the_options[options_answer])
+    except KeyError:
+        print(sys.stderr.write("Opção inválida\n"))
