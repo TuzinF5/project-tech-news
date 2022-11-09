2bf3bf441160d9fd7ce007647633dc4ac3987b2b
Arthur Teixeira <tuzinf5@gmail.com>
2022-11-09 13:28:12 -0300

    feat(function): :sparkles: Creating logic for top_5_news function

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,28 @@
+from tech_news.database import db
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    notices = list(
+        db.news.find(
+            {},
+            {
+                "_id": False,
+                "title": True,
+                "url": True,
+                "comments_count": True,
+            },
+        )
+        .sort("comments_count", -1)
+        .limit(5)
+    )
+
+    response = []
+
+    for notice in notices:
+        response.append((notice["title"], notice["url"]))
+
+    return response
 
 
 # Requisito 11
